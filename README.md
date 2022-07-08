fb7f6ec239f394edc726efddc5f1f1fcd1558f0f
kpaveliev <kpaveliev@gmail.com>
2022-07-08 10:49:33 +0300

    params checks added and readme updated

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
 # SkyPro.Python course
 ## Homework 23. Functional programming
 
-**Content**
+**Description**
 
-1.
+Flask web-server available at http://127.0.0.1:5000/perform_query/
+which could perform queries on the file kept in 'data' folder
+
+To perform a query you should pass filename, command name and command value in a request
+
+Only two commands at a time could be passed (no more, no less)
+
+**Commands available**
+
+1. filter - get lines which contain string passed
+2. map - get data from the passed column only
+3. unique - get unique data only
+4. sort - sort data in ascending or descending order
+5. limit - limit lines to the passed number
+
+**Request examples**
+
+Commands could be passed as a json in POST-request or as request parameters
+
+Option 1 Example:
+{
+    "file_name": "apache_logs.txt",
+    "cmd1": "filter",
+    "value1": "get",
+    "cmd2": "limit",
+    "value2": 5
+}
+
+Option 2 Example:
+http://127.0.0.1:5000/perform_query/?cmd1=filter&value1=GET&cmd2=limit&value2=2
+
 
 Kirill Paveliev\
 July 2022
--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import os
 from flask import Flask, request, abort
 
 import utils
-from constants import DATA_DIR
+from constants import DATA_DIR, COMMANDS
 
 app = Flask(__name__)
 
 
-@app.route("/perform_query/", methods=['POST'])
+@app.route("/perform_query/", methods=['GET', 'POST'])
 def perform_query():
     # Get filename and commands from request
     file_name = request.json.get('file_name')
@@ -17,22 +17,28 @@ def perform_query():
     cmd2 = request.json.get('cmd2') + '_'
     value2 = request.json.get('value2')
 
-    # Check data passed and file path
-    if None in (file_name, cmd1, value1, cmd2, value2):
-        return abort(400, 'Wrong data passed')
+    # Check data passed is correct and file path exists
+    if (
+        None in (file_name, cmd1, value1, cmd2, value2)
+        or cmd1 not in COMMANDS
+        or cmd2 not in COMMANDS
+    ):
+        return abort(400, 'Wrong params passed')
 
     file_path = os.path.join(DATA_DIR, file_name)
-
     if not os.path.exists(file_path):
-        return abort(400, 'No file found')
+        return abort(400, 'Wrong filename passed')
 
-    # Get data from the file and perform commands
-    file_data = open(file_path, 'r')
-    first_result = getattr(utils, cmd1)(file_data, value1)
-    second_result = getattr(utils, cmd2)(first_result, value2)
+    # Perform commands
+    try:
+        with open(file_path, 'r') as file:
+            first_result = getattr(utils, cmd1)(file, value1)
+            second_result = getattr(utils, cmd2)(first_result, value2)
+    except (ValueError, TypeError) as e:
+        abort(400, e)
 
-    return app.response_class(list(second_result), content_type="text/plain")
+    return app.response_class(second_result, content_type="text/plain")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
