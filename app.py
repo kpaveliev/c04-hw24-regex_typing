16139b49f12aa35a8b45df7bc65426504569d741
kpaveliev <kpaveliev@gmail.com>
2022-07-14 11:02:16 +0300

    updated dataclass validation

--- app.py
+++ app.py
@@ -14,22 +14,18 @@ app = Flask(__name__)
 def perform_query():
 
     try:
-        # Get data
+        # Get and validate data, check if file exist
         commands: Commands = CommandsSchema().load(request.args)
         file_path: str = os.path.join(DATA_DIR, commands.file_name)
-
-        # Perform checks
-        if commands.cmd1 not in COMMANDS or commands.cmd2 not in COMMANDS:
-            raise ValueError('Wrong function passed')
 
         if not os.path.exists(file_path):
             raise FileNotFoundError('Wrong filename passed')
 
         # Perform commands passed and return results in a response
         with open(file_path, 'r') as file:
-            first_result = getattr(utils, commands.cmd1 + '_')(file, commands.value1)
-            second_result = getattr(utils, commands.cmd2 + '_')(first_result, commands.value2)
-            return app.response_class('\n'.join([line.strip() for line in second_result]), content_type="text/plain")
+            cmd1_result = getattr(utils, commands.cmd1)(file, commands.value1)
+            cmd2_result = getattr(utils, commands.cmd2)(cmd1_result, commands.value2)
+            return app.response_class('\n'.join([line.strip() for line in cmd2_result]), content_type="text/plain")
 
     except (ValueError, FileNotFoundError, TypeError, IndexError, ValidationError) as e:
         abort(400, e)
--- resources/commands.py
+++ resources/commands.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from typing import Union
 
 import marshmallow_dataclass
+from marshmallow import ValidationError
+
+from resources.constants import COMMANDS
 
 
 @dataclass
@@ -11,6 +14,18 @@ class Commands:
     value1: Union[str, int]
     cmd2: str
     value2: Union[str, int]
+
+    def __post_init__(self):
+        """Validate fields and concatenate commands name with '_'"""
+        if self.cmd1 not in COMMANDS:
+            raise ValidationError(f'Command passed as cmd1 ({self.cmd1}) is not a valid command. '
+                                  f'Only {", ".join(COMMANDS)} allowed')
+        if self.cmd2 not in COMMANDS:
+            raise ValidationError(f'Command passed as cmd2 ({self.cmd2}) is not a valid command. '
+                                  f'Only {", ".join(COMMANDS)} allowed')
+
+        self.cmd1 = self.cmd1 + '_'
+        self.cmd2 = self.cmd2 + '_'
 
 
 CommandsSchema = marshmallow_dataclass.class_schema(Commands)
